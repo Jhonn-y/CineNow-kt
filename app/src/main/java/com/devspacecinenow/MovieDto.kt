4f39956b115158984c2e5b54b89a6892ddb7e07a
jhonatan <jhonatanwsantosdev@gmail.com>
2025-02-23 21:26:45 -0300

    Projeto de Busca de filmes completo

---
+++ app/src/main/java/com/devspacecinenow/MovieDto.kt
@@ -0,0 +1,16 @@
+package com.devspacecinenow
+
+import com.google.gson.annotations.SerializedName
+
+
+
+data class MovieDto(
+    val id: Int,
+    val title: String,
+    val overview: String,
+    @SerializedName("poster_path")
+    val postPath: String,
+){
+    val postFullPath: String
+        get() = "https://image.tmdb.org/t/p/w300$postPath"
+}
---
+++ app/src/main/java/com/devspacecinenow/RetrofitClient.kt
@@ -0,0 +1,41 @@
+package com.devspacecinenow
+
+import okhttp3.OkHttpClient
+import okhttp3.Protocol
+import okhttp3.Request
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+private const val BASE_URL: String = "https://api.themoviedb.org/3/movie/"
+
+object RetrofitClient {
+
+    private val httpClient: OkHttpClient
+        get() {
+            val clientBuilder = OkHttpClient.Builder()
+            val token = BuildConfig.API_KEY
+
+            clientBuilder.addInterceptor { chain ->
+                val original: Request = chain.request()
+                val requestBuilder: Request.Builder = original.newBuilder()
+                    .header("Authorization", "Bearer $token")
+                val request: Request = requestBuilder.build()
+                chain.proceed(request)
+            }
+            clientBuilder.protocols(listOf(Protocol.HTTP_1_1))
+
+            val logging = HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            }
+            clientBuilder.addInterceptor(logging)
+
+            return clientBuilder.build()
+        }
+
+    val retrofitInstance: Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .client(httpClient)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+}
